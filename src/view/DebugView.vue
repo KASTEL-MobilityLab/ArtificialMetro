ee8ef72be3f5d88757824e8f36057295a8ab0098
Daniel Ritz <daniel.ritz@student.kit.edu>
2024-05-10 00:17:02 +0200

    Color coding for carsharing availability

--- src/view/DebugView.vue
+++ src/view/DebugView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import "leaflet/dist/leaflet.css"
 import L from "leaflet"
-import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet"
+import { LMap, LTileLayer, LMarker, LCircleMarker } from "@vue-leaflet/vue-leaflet"
 import { ref, type Ref } from "vue"
 import type { CarsharingStation, Coordinate } from "@/model/vehicles"
 
@@ -20,17 +20,28 @@ channel.onmessage = (message: MessageEvent<CarsharingStation>) => {
   }
 }
 
+function num_to_color(num: number): string {
+  if (num == 1) {
+    return "red"
+  } else if (num == 2) {
+    return "yellow"
+  } else {
+    return "green"
+  }
+}
+
 </script>
 
 <template>
   <div style="height: calc(100vh - 100px); width: 100%">
     <LMap v-model:zoom="zoom" :center="[49.006889, 8.403653]">
       <LTileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base" name="OSM"></LTileLayer>
-      <LMarker v-for="marker, i in stations" v-bind:key="i" :lat-lng="[marker.position.lat, marker.position.lon]"></LMarker>
+      <!-- <LMarker v-for="marker, i in stations" v-bind:key="i" :lat-lng="[marker.position.lat, marker.position.lon]"></LMarker> -->
+      <LCircleMarker v-for="marker, i in stations" v-bind:key="i" :lat-lng="[marker.position.lat, marker.position.lon]"
+        :fill="true" :fill-color="num_to_color(marker.available)" :fill-opacity="1" :stroke="true" :radius="8"
+        color="black"></LCircleMarker>
     </LMap>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
