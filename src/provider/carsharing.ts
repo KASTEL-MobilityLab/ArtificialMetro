53d01dcd16c060716700cd390065be70660a0dc6
Daniel Ritz <daniel.ritz@student.kit.edu>
2024-05-09 09:44:55 +0200

    Add csv library

--- src/provider/carsharing.ts
+++ src/provider/carsharing.ts
@@ -1,9 +1,13 @@
+import * as csv from "web-csv-toolbox"
 
-const endpoint = "https://api.mobidata-bw.de/geoserver/MobiData-BW/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=MobiData-BW%3Asharing_vehicles&CQL_FILTER=form_factor%20%3D%20%27car%27&maxFeatures=20000&outputFormat=csv"
+const endpoint = "https://api.mobidata-bw.de/geoserver/MobiData-BW/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=MobiData-BW%3Asharing_stations_car&maxFeatures=20000&outputFormat=csv"
 const attribution = "NVBW MobiData BW"
 
 export async function load() {
-    const response = await fetch(endpoint);
-    const text = await response.text()
-    console.log(text);
+    const response = await fetch(endpoint)
+
+    for await (const record of csv.parse(response)) {
+        console.log(record)
+    }
+
 }
