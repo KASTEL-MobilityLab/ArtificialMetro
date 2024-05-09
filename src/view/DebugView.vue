55c670f118beb5290f999db76715a4d5dcd4b5c9
Daniel Ritz <daniel.ritz@student.kit.edu>
2024-05-09 10:16:28 +0200

    Include leaflet map

--- src/view/DebugView.vue
+++ src/view/DebugView.vue
@@ -1,8 +1,24 @@
 <script setup lang="ts">
+import "leaflet/dist/leaflet.css"
+import L from "leaflet"
+import { LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet"
+import { ref } from "vue";
+
+// This is needed to correctly load leaflet
+// see https://github.com/vue-leaflet/vue-leaflet/issues/278
+globalThis.L = L;
+
+let zoom = ref(14);
+
 </script>
 
 <template>
-  <h1>Debug</h1>
+  <div style="height: 600px; width: 800px">
+    <LMap v-model:zoom="zoom" :center="[49.006889, 8.403653]">
+      <LTileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" layer-type="base" name="OSM"></LTileLayer>
+      <LMarker :lat-lng="[49.006889, 8.403653]"></LMarker>
+    </LMap>
+  </div>
 </template>
 
 <style scoped>
