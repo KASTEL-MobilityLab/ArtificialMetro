c630f336cd3401c0c8f9173778726fdfc122a2c0
Daniel Ritz <daniel.ritz@student.kit.edu>
2024-05-13 00:23:19 +0200

    Use shared workers instead of dedicated workers

--- src/provider/provider.ts
+++ src/provider/provider.ts
@@ -1,7 +1,7 @@
 export function startCarsharingProvider() {
-    return new Worker(new URL("./carsharing_worker.ts", import.meta.url), { type: "module" })
+    return new SharedWorker(new URL("./carsharing_worker.ts", import.meta.url), { type: "module" })
 }
 
 export function startScooterProvider() {
-    return new Worker(new URL("./scooter_worker.ts", import.meta.url), { type: "module" })
+    return new SharedWorker(new URL("./scooter_worker.ts", import.meta.url), { type: "module" })
 }
