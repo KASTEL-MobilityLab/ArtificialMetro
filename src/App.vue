1a3d0847ecbb5a48bd827610db44c27b033f2b7e
Daniel Ritz <daniel.ritz@student.kit.edu>
2024-06-10 13:26:36 +0200

    Automatic Kiosk mode

--- src/App.vue
+++ src/App.vue
@@ -7,35 +7,55 @@ import ViewSwitcher from './view/ViewSwitcher.vue';
 import { MapIcon, TimerIcon } from 'lucide-vue-next';
 import DebugView from './view/DebugView.vue';
 
+const KIOSK_INTERVAL = 15 * 1000 /*15s*/
+
 const views = [
     { title: "Map", icon: MapIcon, component: DebugView },
     { title: "Timelapse", icon: TimerIcon, component: TimelapseView },
 ]
 const activeView = ref(0)
 const viewComponent = computed(() => views[activeView.value].component)
+const kioskMode = ref(false)
+let kioskModeTicker: number | undefined = undefined
 
 onMounted(() => {
   provider.startAll()
   registerKeyboardSwitcher()
+  startKioskMode()
 })
 
 function switchView(view: number) {
   activeView.value = view
 }
-
+function nextView() {
+  const nextView = (activeView.value + 1) % views.length
+  switchView(nextView)
+}
 
 function registerKeyboardSwitcher() {
   window.addEventListener('keyup', evt => {
     if (!evt.key.match(/[0-9]/)) return
     if (evt.key == "0") {
       // enter automatic kiosk mode
-      // TODO
+      startKioskMode()
     } else {
       // switchView is 0-indexed,
       // but key 0 is special, therefore, we start at key 1
+      stopKioskMode()
       switchView(parseInt(evt.key) - 1)
     }
   })
+}
+
+function startKioskMode() {
+  if (kioskMode.value) return
+  kioskMode.value = true
+  kioskModeTicker = globalThis.setInterval(nextView, KIOSK_INTERVAL)
+}
+function stopKioskMode() {
+  if (!kioskMode.value) return
+  globalThis.clearInterval(kioskModeTicker)
+  kioskMode.value = false
 }
 </script>
 
