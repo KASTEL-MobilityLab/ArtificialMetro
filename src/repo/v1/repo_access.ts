12a9444ab970694748831ff0978ef16ac14ca2ec
Daniel Ritz <daniel.ritz@student.kit.edu>
2024-06-20 21:21:53 +0200

    Refactor repo access server

--- src/repo/v1/repo_access.ts
+++ src/repo/v1/repo_access.ts
@@ -1,6 +1,6 @@
 import type { Storeable } from "@/model/storeable";
 import { BaseRepo } from "@/storage/base_store";
-import { Router } from "express";
+import { Router, type Response } from "express";
 import { DataStore } from "../data_store";
 
 const dateTimeRegex = /[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3}Z/
@@ -9,32 +9,55 @@ export function repoAccess<T extends Storeable>(repo: BaseRepo): Router {
     const router = Router()
 
     router.get('/current', (_, res) => {
-        DataStore.open<T, BaseRepo>(repo, async ds => {
+        requireDatastoreOrFail<T>(res, async ds => {
             const data = await ds.current()
             const latestTimestamp = await ds.getLatestTimestamp() ?? new Date()
-            res.header('Etag', latestTimestamp.toISOString())
-            res.json(data)
+            sendResult<T>(res, data, latestTimestamp.toISOString())
         })
     })
 
     router.get('/:timestamp', (req, res) => {
         const timestamp = req.params.timestamp
         if (timestamp.match(dateTimeRegex)) {
-            DataStore.open<T, BaseRepo>(repo, async ds => {
-                const data = await ds.forTimestamp(new Date(timestamp))
-                res.header('Etag', timestamp)
-                res.json(data)
-            }).catch(() => {
-                res.sendStatus(501)
-            })
+            sendDataForTimestamp<T>(res, timestamp);
         } else {
-            res.sendStatus(404)
+            sendNotFound(res)
         }
     })
 
-    router.get('/*', (req, res) => {
-        res.sendStatus(404)
+    function sendDataForTimestamp<T extends Storeable>(res: any, timestamp: string) {
+        requireDatastoreOrFail<T>(res, async (ds) => {
+            const data = await ds.forTimestamp(new Date(timestamp));
+            sendResult<T>(res, data, timestamp);
+        });
+    }
+
+    router.get('/*', (_, res) => {
+        sendNotFound(res)
     })
 
+
+
+    function requireDatastoreOrFail<T extends Storeable>(res: any, callback: (self: DataStore<T, BaseRepo>) => Promise<void>) {
+        DataStore
+            .open<T, BaseRepo>(repo, callback)
+            .catch(() => {
+                res.sendStatus(501)
+            })
+    }
+
     return router
+}
+
+
+
+
+
+function sendResult<T extends Storeable>(res: any, data: T[], timestamp: string) {
+    res.header('Etag', timestamp);
+    res.json(data);
+}
+
+function sendNotFound(res: any) {
+    res.sendStatus(401)
 }
