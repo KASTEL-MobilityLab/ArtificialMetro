f45ab93b4107f054234477c4ead7143c721ef17a
Daniel Ritz <daniel.ritz@student.kit.edu>
2024-06-20 22:19:58 +0200

    Refactored server repo routing

--- server.ts
+++ server.ts
@@ -1,11 +1,11 @@
 import express from "express"
 import ViteExpress from "vite-express"
-import * as v1 from "./src/repo/v1"
+import v1 from "./src/repo/v1"
 import {SHARE_ENV, Worker} from "node:worker_threads"
 
 const tsx = new URL(import.meta.resolve('tsx/cli'))
 new Worker(tsx, {env: SHARE_ENV, argv: ["./src/repo/updater.js"]})
 
 const app = express()
-v1.use(app)
+app.use('/v1', v1)
 ViteExpress.listen(app, 3000)
--- src/repo/v1.ts
+++ src/repo/v1.ts
@@ -1,74 +1,11 @@
 import type { Scooter, CarsharingStation } from "@/model/vehicles"
-import type { Express } from "express"
-import { DataStore } from "./data_store"
+import { Router } from "express"
 import { BaseRepo } from "@/storage/base_store"
-import {repoAccess} from "./v1/repo_access"
+import { repoAccess } from "./v1/repo_access"
 
-export function use(app: Express) {
-    app.use('/v1/carsharing_stations', repoAccess<CarsharingStation>(BaseRepo.CarsharingStations))
-    app.use('/v1/scooters', repoAccess<Scooter>(BaseRepo.Scooters))
 
-    app.get('/v1/sample', (_, res) => {
+const router = Router()
+router.use('/carsharing_stations', repoAccess<CarsharingStation>(BaseRepo.CarsharingStations))
+router.use('/scooters', repoAccess<Scooter>(BaseRepo.Scooters))
 
-        const sample: CarsharingStation[] = [
-            {
-                id: "001",
-                available: 1,
-                timestamp: new Date(),
-                position: { lat: 0, lon: 0 },
-            },
-        ]
-
-        res.type('json')
-        res.send(JSON.stringify(sample))
-    })
-    app.get('/v1/init', async (_, res) => {
-
-        DataStore.open<CarsharingStation, BaseRepo>(BaseRepo.CarsharingStations, async c => {
-            await c.store([
-                {
-                    id: "001",
-                    available: 1,
-                    timestamp: new Date(),
-                    position: { lat: 0, lon: 0 },
-                },
-                {
-                    id: "002",
-                    available: 3,
-                    timestamp: new Date(),
-                    position: { lat: 2, lon: 2 },
-                }
-            ])
-        })
-        res.send("Ok")
-    })
-    app.get('/v1/get', async (_, res) => {
-        DataStore.open<CarsharingStation, BaseRepo>(BaseRepo.CarsharingStations, async c => {
-            const timestamps = await c.getAvailableTimestamps()
-            res.type('json')
-            res.send(JSON.stringify(timestamps))
-        })
-    })
-    app.get('/v1/current', async (_, res) => {
-        DataStore.open<CarsharingStation, BaseRepo>(BaseRepo.CarsharingStations, async c => {
-            const items = await c.current()
-            res.type('json')
-            res.send(JSON.stringify(items))
-        })
-    })
-    app.get('/v1/scooters', async (_, res) => {
-        DataStore.open<Scooter, BaseRepo>(BaseRepo.Scooters, async c => {
-            const items = await c.current()
-            res.type('json')
-            res.send(JSON.stringify(items))
-        })
-    })
-
-    app.get('/v1/all', async (_, res) => {
-        DataStore.open<CarsharingStation, BaseRepo>(BaseRepo.CarsharingStations, async c => {
-            const items = await c.all()
-            res.type('json')
-            res.send(JSON.stringify(items))
-        })
-    })
-}
+export default router
