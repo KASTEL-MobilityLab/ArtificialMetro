385eddd6d2906f7096787cfbcaf52316d1bb8b97
Daniel Ritz <daniel.ritz@student.kit.edu>
2024-06-20 23:12:42 +0200

    Group workers in worker directory

--- src/provider/provider.ts
+++ src/provider/provider.ts
@@ -4,17 +4,3 @@ export interface Provider<T extends Storeable> {
     attribution(): string
     fetch(): Promise<T[]>
 }
-
-export function startAll() {
-    startSyncProvider()
-    startCleanupProvider()
-}
-
-export function startCleanupProvider() {
-    return new SharedWorker(new URL("./cleanup_worker.ts", import.meta.url), { type: "module" })
-}
-
-export function startSyncProvider() {
-    return new SharedWorker(new URL("./sync_worker.ts", import.meta.url), { type: "module" })
-}
-
