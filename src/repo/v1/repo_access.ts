8014d1fdaf1ac934fad8a4e80bf808cc5cf5e815
Daniel Ritz <daniel.ritz@student.kit.edu>
2024-06-20 21:36:16 +0200

    Request timestamp ranges

--- src/repo/v1/repo_access.ts
+++ src/repo/v1/repo_access.ts
@@ -1,6 +1,6 @@
 import type { Storeable } from "@/model/storeable";
 import { BaseRepo } from "@/storage/base_store";
-import { Router, type Response } from "express";
+import { Router } from "express";
 import { DataStore } from "../data_store";
 
 const dateTimeRegex = /[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3}Z/
@@ -25,12 +25,41 @@ export function repoAccess<T extends Storeable>(repo: BaseRepo): Router {
         }
     })
 
+    router.get('/:from/:until', (req, res) => {
+        const from = req.params.from
+        const until = req.params.until
+        if (from.match(dateTimeRegex) && until.match(dateTimeRegex)) {
+            const fromDate = new Date(from)
+            const untilDate = new Date(until)
+            if (from <= until) {
+                sendDataForRange<T>(res, fromDate, untilDate)
+            } else {
+                sendClientError(res)
+            }
+        } else {
+            sendNotFound(res)
+        }
+    })
+
     function sendDataForTimestamp<T extends Storeable>(res: any, timestamp: string) {
-        requireDatastoreOrFail<T>(res, async (ds) => {
+        requireDatastoreOrFail<T>(res, async ds => {
             const data = await ds.forTimestamp(new Date(timestamp));
             sendResult<T>(res, data, timestamp);
         });
     }
+
+    function sendDataForRange<T extends Storeable>(res: any, fromDate: Date, untilDate: Date) {
+        requireDatastoreOrFail<T>(res, async ds => {
+            const data: T[] = []
+            let currentDate = fromDate
+            while (currentDate <= untilDate) {
+                data.push(...await ds.forTimestamp(currentDate))
+                currentDate = new Date(currentDate.getTime() + 5 * 60 * 1000 /* 5 min */)
+            }
+            sendResult<T>(res, data, untilDate.toISOString())
+        })
+    }
+
 
     router.get('/*', (_, res) => {
         sendNotFound(res)
@@ -59,5 +88,9 @@ function sendResult<T extends Storeable>(res: any, data: T[], timestamp: string)
 }
 
 function sendNotFound(res: any) {
+    res.sendStatus(404)
+}
+
+function sendClientError(res: any) {
     res.sendStatus(401)
 }
