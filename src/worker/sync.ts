ede381aa61d32dd62ee7a713426add08924c520f
Daniel Ritz <daniel.ritz@student.kit.edu>
2024-07-03 17:06:54 +0200

    Wrap loading initial and inremental data in a class

--- src/worker/sync.ts
+++ src/worker/sync.ts
@@ -4,32 +4,63 @@ import { type Bike, type CarsharingStation, type Scooter } from "@/model/vehicle
 import { BaseStore } from "@/storage/base_store"
 import { BaseRepo } from "@/model/repos"
 
+type LoadedHandler = (repo: BaseRepo) => void
+
+class SyncRepo<T extends Storeable> {
+    private repo: BaseRepo
+    private etag: string|null = null
+    private loadedHandler: LoadedHandler = () => {}
+
+    constructor(repo: BaseRepo) {
+        this.repo = repo
+    }
+
+    onLoaded(handler: LoadedHandler) {
+        this.loadedHandler = handler
+    }
+
+    async loadInitialData() {
+        const currentDate = normalizeTimestamp(new Date())
+        const lastHour = new Date(currentDate.getTime() - 1 * 60 * 60 * 1000 /* 1h */)
+
+        const url = `/v1/${this.repo}/${lastHour.toISOString()}/${currentDate.toISOString()}`
+        const { data, etag } = await fetchData<T>(url)
+        storeData(data, this.repo)
+        this.loadedHandler(this.repo)
+        this.etag = etag
+    }
+
+    async loadIncrementalData() {
+        const url = `/v1/${this.repo}/current`
+        const { data, etag } = await fetchData<T>(url)
+        storeData(data, this.repo)
+        this.etag = etag
+    }
+}
+
 export async function sync() {
-    loadIncrementalData<CarsharingStation>(BaseRepo.CarsharingStations)
-    loadIncrementalData<Scooter>(BaseRepo.Scooters)
-    loadIncrementalData<Bike>(BaseRepo.Bikes)
+    const carsharing = new SyncRepo<CarsharingStation>(BaseRepo.CarsharingStations)
+    carsharing.loadIncrementalData()
+
+    const scooters = new SyncRepo<Scooter>(BaseRepo.Scooters)
+    scooters.loadIncrementalData()
+
+    const bikes = new SyncRepo<Bike>(BaseRepo.Bikes)
+    bikes.loadIncrementalData()
 }
 
 export async function initial_sync(reportStatus: { (repo: BaseRepo): void }) {
-    loadInitialData<CarsharingStation>(BaseRepo.CarsharingStations, reportStatus)
-    loadInitialData<Scooter>(BaseRepo.Scooters, reportStatus)
-    loadInitialData<Bike>(BaseRepo.Bikes, reportStatus)
-}
+    const carsharing = new SyncRepo<CarsharingStation>(BaseRepo.CarsharingStations)
+    carsharing.onLoaded(reportStatus)
+    carsharing.loadInitialData()
 
-async function loadInitialData<T extends Storeable>(repo: BaseRepo, reportStatus: { (repo: BaseRepo): void }) {
-    const currentDate = normalizeTimestamp(new Date())
-    const lastHour = new Date(currentDate.getTime() - 1 * 60 * 60 * 1000 /* 1h */)
+    const scooters = new SyncRepo<Scooter>(BaseRepo.Scooters)
+    scooters.onLoaded(reportStatus)
+    scooters.loadInitialData()
 
-    const url = `/v1/${repo}/${lastHour.toISOString()}/${currentDate.toISOString()}`
-    const { data, etag } = await fetchData<T>(url)
-    storeData(data, repo)
-    reportStatus(repo)
-}
-
-async function loadIncrementalData<T extends Storeable>(repo: BaseRepo) {
-    const url = `/v1/${repo}/current`
-    const { data, etag } = await fetchData<T>(url)
-    storeData(data, repo)
+    const bikes = new SyncRepo<Bike>(BaseRepo.Bikes)
+    bikes.onLoaded(reportStatus)
+    bikes.loadInitialData()
 }
 
 async function fetchData<T extends Storeable>(url: string): Promise<{ data: T[], etag: string | null }> {
