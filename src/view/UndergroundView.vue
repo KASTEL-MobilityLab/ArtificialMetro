3d1563bb52ee6dbaa177b65bb91a58f9f3ec474e
Daniel Ritz <daniel.ritz@student.kit.edu>
2024-08-20 22:25:58 +0200

    Visualize train between two stations

--- src/view/UndergroundView.vue
+++ src/view/UndergroundView.vue
@@ -7,9 +7,12 @@ import { TileProvider } from './map/tile_provider';
 import { SpriteManager } from './map/sprite_manager';
 import { tramStation } from '@/model/brands';
 import type { Marker } from './map/tiles';
-import type { Coordinate } from '@/model/vehicles';
-import { onMounted, ref } from 'vue';
+import type { TramDeparture, Coordinate } from '@/model/vehicles';
 import LineRenderer from './map/LineRenderer.vue';
+import { computed, onMounted, ref, watch } from 'vue';
+import { BaseStore } from '@/storage/base_store';
+import { BaseRepo } from '@/model/repos';
+import type { CacheRepo } from '@/storage/cache_repo';
 
 const VELVET = "#902C3E" // Velvet Underground
 
@@ -17,11 +20,11 @@ defineProps<{
     bus: SwitchBusReceiver,
 }>()
 
+let tramRepo = ref<CacheRepo<TramDeparture, BaseRepo> | null>(null)
+
 let zoom = 16
 let center = { lon: 8.41, lat: 49.0054 }
 const tileProvider = new TileProvider("https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}.png")
-
-
 
 const stations: { [key: string]: Coordinate } = {
     marktplatzKaiserstrasse: { lat: 49.009656, lon: 8.403112 },
@@ -32,7 +35,7 @@ const stations: { [key: string]: Coordinate } = {
     kronenplatz: { lat: 49.009249, lon: 8.410788 },
     durlacherTor: { lat: 49.008749, lon: 8.418109 },
 }
-const stationConnectios = [
+const stationConnections = [
     { start: stations.europaplatz, end: stations.marktplatzKaiserstrasse },
     { start: stations.marktplatzKaiserstrasse, end: stations.kronenplatz },
     { start: stations.kronenplatz, end: stations.durlacherTor },
@@ -57,6 +60,77 @@ spriteManager.fetchSprite({
     })
 })
 
+const currentTime = ref(new Date())
+function updateTime() {
+    currentTime.value = new Date()
+}
+
+type Section = [TramDeparture, TramDeparture]
+const sections: Section[] = []
+const currentTrains = ref<Coordinate[]>([])
+const currentTrainMarkers = computed<Marker[]>(() => {
+    return currentTrains.value.map(t => {
+        return {
+            position: t,
+            sprite: "station",
+        }
+    })
+})
+
+onMounted(async () => {
+    const baseStore = await BaseStore.open()
+    tramRepo.value = baseStore.repo<TramDeparture>(BaseRepo.TramDepartures)
+    tramRepo.value.onUpdate(() => updateSections())
+    updateSections()
+
+    setInterval(updateTime, 2 * 1000 /*2s*/)
+})
+
+
+async function updateSections() {
+    if (tramRepo.value == null) return
+    const departures = await tramRepo.value.current()
+    const startDepartures = departures.filter(d => d.station == "7001001")
+    const endDepartures = departures.filter(d => d.station == "7001002")
+
+    sections.splice(0, sections.length, ...startDepartures
+        .map(d => matchDeparture(d, endDepartures))
+        .filter(d => d != null)
+        .map(d => d as Section)
+    )
+}
+
+function matchDeparture(departure: TramDeparture, otherDepartures: TramDeparture[]): Section | null {
+    for (const otherDeparture of otherDepartures) {
+        if (otherDeparture.trainNumber == departure.trainNumber) {
+            return [departure, otherDeparture]
+        }
+    }
+    return null
+}
+
+watch(() => currentTime.value, time => {
+    const activeTrains = sections.filter(s => {
+        const departureInPast = new Date(s[0].realtime) < time
+        const arrivalInFuture = new Date(s[1].realtime) > time
+        return departureInPast && arrivalInFuture
+    })
+    currentTrains.value = activeTrains.map(train => {
+        const startTime = new Date(train[0].realtime)
+        const endTime = new Date(train[1].realtime)
+        const timeDifference = endTime.getTime() - startTime.getTime()
+        const factor = (currentTime.value.getTime() - startTime.getTime()) / timeDifference
+        const currentPosition = { 
+            lat: stations.durlacherTor.lat - (stations.durlacherTor.lat - stations.kronenplatz.lat) * factor, 
+            lon: stations.durlacherTor.lon - (stations.durlacherTor.lon - stations.kronenplatz.lon) * factor, 
+        }
+        return currentPosition
+    })
+    console.log(activeTrains, currentTrains.value)
+})
+
+
+
 </script>
 
 <template>
@@ -64,8 +138,10 @@ spriteManager.fetchSprite({
 
         <LocationFrame :center="center" :zoom="zoom" #default="data">
             <TileRenderer v-bind="data" :tiles="tileProvider"></TileRenderer>
-            <LineRenderer v-bind="data" :lines="stationConnectios" :color="VELVET"></LineRenderer>
-            <MarkerRenderer v-bind="data" :marker="stationMarker" :sprites="spriteManager" :size="20"></MarkerRenderer>
+            <LineRenderer v-bind="data" :lines="stationConnections" :color="VELVET"></LineRenderer>
+            <!-- <MarkerRenderer v-bind="data" :marker="stationMarker" :sprites="spriteManager" :size="20"></MarkerRenderer> -->
+            <MarkerRenderer v-bind="data" :marker="currentTrainMarkers" :sprites="spriteManager" :size="20">
+            </MarkerRenderer>
         </LocationFrame>
 
     </div>
