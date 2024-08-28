a6de90ce8df132f91fe2bb5e58981de29116cf4c
Daniel Ritz <daniel.ritz@student.kit.edu>
2024-08-28 16:37:57 +0200

    Play random landscape videos

--- src/view/LandscapeView.vue
+++ src/view/LandscapeView.vue
@@ -1,14 +1,67 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
 import type { SwitchBusReceiver } from './switch_bus';
 
-defineProps<{
+const props = defineProps<{
     bus: SwitchBusReceiver,
 }>()
+
+type VideoUrl = string
+const availableVideos = ref<VideoUrl[]>([])
+const currentVideo = ref<VideoUrl|undefined>(undefined)
+const video = ref<HTMLVideoElement>()
+
+async function loadAvailableVideos() {
+    const response = await fetch('/v1/videos')
+    if (!response.ok) return
+    const list = await response.json()
+    availableVideos.value = list as VideoUrl[]
+}
+
+async function selectNextVideo() {
+    if (availableVideos.value.length == 0) {
+        await loadAvailableVideos()
+    }
+    const lastVideo = currentVideo.value
+    const num = Math.floor(Math.random() * availableVideos.value.length)
+    currentVideo.value = undefined
+    currentVideo.value = availableVideos.value[num]
+    if (currentVideo.value == lastVideo) {
+        playVideo()
+    }
+}
+
+function playVideo() {
+    try {
+        const duration = video.value!.duration
+        let start = 0
+        if (!isNaN(duration)) {
+            start = Math.max(0, Math.floor(Math.random() * duration) - 90 /*90s*/)
+        }
+        video.value!.currentTime = start
+        video.value!.play()
+    } catch(_) {
+        console.log('cannot play video')
+    }
+}
+
+function stopVideo() {
+    video.value?.pause()
+}
+
+onMounted(() => {
+    props.bus.onResume(selectNextVideo)
+    props.bus.onSuspend(stopVideo)
+    video.value!.addEventListener('loadedmetadata', playVideo)
+})
+
 </script>
 
 <template>
     <div class="window-frame">
-        <div class="window-view"></div>
+        <div class="window-view">
+            <video ref="video" :src="currentVideo" muted controls="false"></video>
+        </div>
     </div>
 </template>
 
@@ -33,8 +86,10 @@ defineProps<{
     right: 50px;
     bottom: 30px;
     border-radius: 20px;
-    box-shadow: 0 0 5px rgba(0,0,0,0.5) inset;
 
-    background: cornflowerblue;
+    overflow: hidden;
+}
+.window-view video {
+    height: 100%;
 }
 </style>
